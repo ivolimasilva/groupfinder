bf1158d75ed328a86aab8b1f278bdb5bc9a718ec
Ivo Lima da Silva <ivo.lima.da.silva@live.com.pt>
2015-09-02 18:59:24 +0100

    Update to accept only FEUP e-mails.

--- private/config/stringvalidator.js
+++ private/config/stringvalidator.js
@@ -6,8 +6,14 @@
 
     exports.checkvalidemail = function (email) {
 
-        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
+        //var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        //return re.test(email);
+
+        if (email.search('@fe.up.pt') === email.length - 9) {
+            return true;
+        } else {
+            return false;
+        }
 
     };
 
